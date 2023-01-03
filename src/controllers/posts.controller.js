fefa43fd62293416d0c6dfb53d620194bd31dbd4
carloshnp <carloshnpwork@gmail.com>
2023-01-03 19:34:01 -0300

    feat: get posts controller

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,1 +1,24 @@
 import { db } from "../database/db.js";
+
+export async function getPosts(req, res) {
+  try {
+    const posts = await db.query(
+      `SELECT 
+           u.id, 
+           u.name, 
+           u.image,
+           p.id, 
+           p.text, 
+           p.link, 
+           p."createdAt" 
+       FROM posts AS p 
+       JOIN users AS u ON p."userId" = u.id 
+       ORDER BY p."createdAt" DESC 
+       LIMIT 20;`
+    );
+    res.send(posts.rows).status(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { getPosts } from "../controllers/posts.controller.js";
 
 const router = Router()
 
-router.get('/timeline')
+router.get('/timeline', getPosts)
 router.post('/post')
 router.delete('/post/:id')
 router.put('/post/:id')
