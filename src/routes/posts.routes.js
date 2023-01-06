d1f2624263838bd04e5e3cfca6a7f61fed50f444
lucaspaulii <lucaspauli22@gmail.com>
2023-01-06 00:29:24 -0300

    feat: route get all posts from a hashtag

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
-import { getPosts } from "../controllers/posts.controller.js";
+import { getPosts, getPostsFromHashtag } from "../controllers/posts.controller.js";
 import { createPostController } from "../controllers/posts.controller.js";
 import { validateCreatePost } from "../middlewares/validateCreatePost.js";
 import { validateToken } from "../middlewares/validateToken.middleware.js";
 
-const router = Router()
+const router = Router();
 
-router.get('/timeline', getPosts)
-router.post('/post', validateToken ,validateCreatePost, createPostController)
-router.delete('/post/:id')
-router.put('/post/:id')
+router.get("/timeline", getPosts);
+router.get("/posts/:hashtagName", getPostsFromHashtag);
+router.post("/post", validateToken, validateCreatePost, createPostController);
+router.delete("/post/:id");
+router.put("/post/:id");
 
-export default router
+export default router;
