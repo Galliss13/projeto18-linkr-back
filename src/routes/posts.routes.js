a44ce9559a6ec45988c0579295a752ecec1df75d
carloshnp <carloshnpwork@gmail.com>
2023-01-08 19:19:09 -0300

    feat: get user posts by params

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   getPosts,
   getPostsFromHashtag,
+  getUserPosts,
 } from "../controllers/posts.controller.js";
 import { createPostController, deletePost, editPost } from "../controllers/posts.controller.js";
 import { deleteHashtags, validateCreatePost, validateEditPost } from "../middlewares/posts.middleware.js";
@@ -10,6 +11,7 @@ import { validateToken } from "../middlewares/validateToken.middleware.js";
 const router = Router();
 
 router.get("/timeline", getPosts);
+router.get("/user/:id", getUserPosts);
 router.get("/posts/:hashtagName", validateToken, getPostsFromHashtag);
 router.post("/post", validateToken, validateCreatePost, createPostController);
 router.delete("/post/:id",validateToken, deleteHashtags, deletePost);
