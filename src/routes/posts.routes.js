db8d21f3e8dd933808cb99765ca984b05de66a65
carloshnp <carloshnpwork@gmail.com>
2023-01-09 12:32:56 -0300

    feat: auth at get timeline posts and get userposts

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -2,19 +2,27 @@ import { Router } from "express";
 import {
   getPosts,
   getPostsFromHashtag,
-  getUserPosts,
+  getUserPosts
 } from "../controllers/posts.controller.js";
-import { createPostController, deletePost, editPost } from "../controllers/posts.controller.js";
-import { deleteHashtags, validateCreatePost, validateEditPost } from "../middlewares/posts.middleware.js";
+import {
+  createPostController,
+  deletePost,
+  editPost
+} from "../controllers/posts.controller.js";
+import {
+  deleteHashtags,
+  validateCreatePost,
+  validateEditPost
+} from "../middlewares/posts.middleware.js";
 import { validateToken } from "../middlewares/validateToken.middleware.js";
 
 const router = Router();
 
-router.get("/timeline", getPosts);
-router.get("/user/:id", getUserPosts);
+router.get("/timeline", validateToken, getPosts);
+router.get("/user/:id", validateToken, getUserPosts);
 router.get("/posts/:hashtagName", validateToken, getPostsFromHashtag);
 router.post("/post", validateToken, validateCreatePost, createPostController);
-router.delete("/post/:id",validateToken, deleteHashtags, deletePost);
-router.put("/post/:id",validateToken, validateEditPost, editPost);
+router.delete("/post/:id", validateToken, deleteHashtags, deletePost);
+router.put("/post/:id", validateToken, validateEditPost, editPost);
 
 export default router;
