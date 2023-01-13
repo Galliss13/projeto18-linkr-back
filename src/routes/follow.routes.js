88d184e83c23ba75c25aef579bd146886909330d
Galliss13 <matheusgalindo1313@gmail.com>
2023-01-13 16:37:03 -0300

    fix: getAllUserFollows without validate token

--- src/routes/follow.routes.js
+++ src/routes/follow.routes.js
@@ -5,7 +5,7 @@ import { validateToken } from "../middlewares/validateToken.middleware.js";
 
 const router = Router()
 
-router.get('/follow/:userId', validateToken, getAllUserFollows)
+router.get('/follow/:userId', getAllUserFollows)
 router.get('/follow/:followerId/:followedId',validateToken, checkExistUsers, checkFollow )
 router.post('/follow-user', validateToken, checkExistUsers,  followOrUnfollow)
 
