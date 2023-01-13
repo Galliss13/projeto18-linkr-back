b472bd503285cc8d306ac8a3cc314d04d9e49f27
lucaspaulii <lucaspauli22@gmail.com>
2023-01-13 01:45:09 -0300

    feat: delete repost by id route

--- src/controllers/reposts.controller.js
+++ src/controllers/reposts.controller.js
@@ -1,4 +1,7 @@
-import { postRepost } from "../repositories/reposts.repositories.js";
+import {
+  deleteRepostById,
+  postRepost,
+} from "../repositories/reposts.repositories.js";
 
 export async function repostController(req, res) {
   const { postId, userId, repostedAt } = req.repostObj;
@@ -6,6 +9,18 @@ export async function repostController(req, res) {
   try {
     await postRepost(postId, userId, repostedAt);
     return res.sendStatus(201);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+}
+
+export async function deleteRepost(req, res) {
+  const { repostId } = req.params;
+  const { userId } = res.locals;
+  try {
+    const deleted = await deleteRepostById(repostId, userId);
+    if (!deleted) return res.sendStatus(404);
+    return res.sendStatus(200);
   } catch (error) {
     return res.status(400).send(error);
   }
